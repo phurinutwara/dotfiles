9e343a1eb2c688471e3e7e7eeaecdeddd6a5fff5
phurinutw <phurinut.wara@gmail.com>
2023-11-27 17:01:12 +0700

    Add powerlevel10k

--- setup_omz.zsh
+++ setup_omz.zsh
@@ -16,5 +16,6 @@ else
 	echo -n "installing custom plugins: "
 	git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 	echo "DONE"
 fi
