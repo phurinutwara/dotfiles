23fff1069ddd21929957eca5a0f678d1a76cbe9f
phurinutw <phurinut.wara@gmail.com>
2023-11-27 17:17:29 +0700

    Add tmux plugin manager

---
+++ setup_tpm.zsh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+TPM_PATH=~/.tmux/plugins/tpm
+
+echo "\n<<< Starting Tmux Plugin Manager Setup >>>\n"
+
+if exa $TPM_PATH >/dev/null 2>&1; then
+	echo "tpm exists, skipping install"
+else
+	echo "tpm doesn't exist, continuing with install"
+	git clone https://github.com/tmux-plugins/tpm $TPM_PATH
+fi
