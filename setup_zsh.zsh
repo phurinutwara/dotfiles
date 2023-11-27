870baab44288c644c7b0ca18902ed5587702606b
phurinutw <phurinut.wara@gmail.com>
2023-11-27 05:06:07 +0000

    Change sh to brew-installed zsh with eval which command

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -5,7 +5,9 @@ echo "\n<<< Starting ZSH Setup >>>\n"
 # Installation unnecessary; it's in the Brewfile.
 
 echo "Enter superuser (sudo) password to edit /etc/shells"
-echo '/home/linuxbrew/.linuxbrew/bin/zsh' | sudo tee -a '/etc/shells'
+echo $(which -a zsh | head -1) | sudo tee -a '/etc/shells'
 
 echo "Enter user password to change login shell"
-chsh -s '/home/linuxbrew/.linuxbrew/bin/zsh'
+chsh -s $(which -a zsh | head -1)
+
+sudo ln -sfv $(which -a zsh | head -1) /private/var/select/sh
