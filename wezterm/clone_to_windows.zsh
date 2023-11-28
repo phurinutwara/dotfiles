eaa1365eaaeb81bf5e0ea3690717a701701dee7b
phurinutw <phurinut.wara@gmail.com>
2023-11-28 19:20:34 +0700

    update wezterm config
    also add zsh script to update window's wezterm config file

---
+++ wezterm/clone_to_windows.zsh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+
+SCRIPT_DIR=$(dirname "$0")
+cp $SCRIPT_DIR/wezterm.lua /mnt/c/Users/phuri/.wezterm.lua
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -11,12 +11,34 @@ if wezterm.config_builder then
 end
 
 -- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
-config.color_scheme = 'Catppuccin Mocha'
-config.window_background_opacity = 0.85
 config.default_domain = 'WSL:Ubuntu-20.04'
+
+config.color_scheme = 'Catppuccin Mocha'
 config.font = wezterm.font 'JetBrains Mono'
+config.font_size = 13.0
+
+config.enable_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+config.window_background_opacity = 0.90
+config.macos_window_background_blur = 30
+
+config.window_decorations = 'RESIZE'
+config.keys = {
+  {
+    key = 'f',
+    mods = 'CTRL',
+    action = wezterm.action.ToggleFullScreen,
+  },
+}
+
+config.mouse_bindings = {
+  -- Ctrl-click will open the link under the mouse cursor
+  {
+    event = { Up = { streak = 1, button = 'Left' } },
+    mods = 'CTRL',
+    action = wezterm.action.OpenLinkAtMouseCursor,
+  },
+}
 
 -- and finally, return the configuration to wezterm
 return config
