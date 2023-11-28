0e61135bfcf33536e36b77418552cfc842b37c4f
phurinutw <phurinut.wara@gmail.com>
2023-11-29 04:24:43 +0700

    add inotify-tools and config wezterm script

--- wezterm/clone_to_windows.zsh
+++ wezterm/clone_to_windows.zsh
@@ -1,4 +1,6 @@
 #!/usr/bin/env zsh
 
 SCRIPT_DIR=$(dirname "$0")
-cp $SCRIPT_DIR/wezterm.lua /mnt/c/Users/phuri/.wezterm.lua
+while inotifywait -r $SCRIPT_DIR/wezterm.lua; do
+	cp $SCRIPT_DIR/wezterm.lua /mnt/c/Users/phuri/.wezterm.lua
+done
