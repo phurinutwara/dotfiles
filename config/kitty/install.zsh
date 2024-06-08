fe17f30ffd8558e10e035f88921d4d8b1ecb2c4f
Phurinut Waranich <phurinut.wara@gmail.com>
2024-06-09 00:57:21 +0700

    kitty: wrap installation after prompting

--- config/kitty/install.zsh
+++ config/kitty/install.zsh
@@ -1,14 +1,27 @@
-sudo apt install wslu
+#!/bin/bash
 
-sudo add-apt-repository ppa:kisak/kisak-mesa
-sudo apt upgrade
-sudo apt install mesa-utils vulkan-tools
+while true; do
+    read -p "Do you wish to install this program [y/n]? " yn
+    case $yn in
+        [Yy]* ) make install; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 
-# install emoji font
-sudo apt install fonts-noto-color-emoji
+function install(){
+	sudo apt install wslu
 
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin \
-	installer=nightly \
-	launch=n
+	sudo add-apt-repository ppa:kisak/kisak-mesa
+	sudo apt upgrade
+	sudo apt install mesa-utils vulkan-tools
 
-sudo ln -s ~/.local/kitty.app/bin/kitty /usr/bin/kitty
+	# install emoji font
+	sudo apt install fonts-noto-color-emoji
+
+	curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin \
+		installer=nightly \
+		launch=n
+
+	sudo ln -s ~/.local/kitty.app/bin/kitty /usr/bin/kitty
+}
