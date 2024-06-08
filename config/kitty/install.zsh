bd3f9c07cae45d7014017c3d85e9dde99cfb44ba
Phurinut Waranich <phurinut.wara@gmail.com>
2024-06-09 02:15:37 +0700

    kitty: update kitty installation

--- config/kitty/install.zsh
+++ config/kitty/install.zsh
@@ -8,20 +8,3 @@ while true; do
         * ) echo "Please answer yes or no.";;
     esac
 done
-
-function install(){
-	sudo apt install wslu
-
-	sudo add-apt-repository ppa:kisak/kisak-mesa
-	sudo apt upgrade
-	sudo apt install mesa-utils vulkan-tools
-
-	# install emoji font
-	sudo apt install fonts-noto-color-emoji
-
-	curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin \
-		installer=nightly \
-		launch=n
-
-	sudo ln -s ~/.local/kitty.app/bin/kitty /usr/bin/kitty
-}
