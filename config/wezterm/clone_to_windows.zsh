e63ac8f62a869f31a4f37398073b4f4229c9e4f2
Phurinut Waranich <phurinut.wara@gmail.com>
2024-06-10 20:52:58 +0700

    wezterm: update clone configs to window script

--- config/wezterm/clone_to_windows.zsh
+++ config/wezterm/clone_to_windows.zsh
@@ -1,6 +1,9 @@
 #!/usr/bin/env zsh
 
+HOST_USER=phurinutwara
 SCRIPT_DIR=$(dirname "$0")
-while inotifywait -r $SCRIPT_DIR/wezterm.lua; do
-	cp $SCRIPT_DIR/wezterm.lua /mnt/c/Users/phuri/.wezterm.lua
+while inotifywait -r $SCRIPT_DIR/**; do
+	cp -vr $SCRIPT_DIR/wezterm.lua /mnt/c/Users/$HOST_USER/.wezterm.lua
+	echo ""
+	cp -vr $SCRIPT_DIR /mnt/c/Users/$HOST_USER/.wezterm/
 done
