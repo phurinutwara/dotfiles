929e45a70614654f0c8d8309ff8952ac1e2fd6f2
Phurinut Waranich <phurinut.wara@gmail.com>
2024-06-11 06:59:46 +0700

    wezterm: complete new method to clone configs to wsl's windows host

--- config/wezterm/clone_to_windows.zsh
+++ config/wezterm/clone_to_windows.zsh
@@ -1,9 +1,47 @@
 #!/usr/bin/env zsh
 
-HOST_USER=phurinutwara
 SCRIPT_DIR=$(dirname "$0")
-while inotifywait -r $SCRIPT_DIR/**; do
-	cp -vr $SCRIPT_DIR/wezterm.lua /mnt/c/Users/$HOST_USER/.wezterm.lua
-	echo ""
-	cp -vr $SCRIPT_DIR /mnt/c/Users/$HOST_USER/.wezterm/
-done
+SCRIPT_NAME=$(basename "$0")
+
+HOST_USERPATH=$(wslupath -H)
+
+MODULE_FILES=(
+	$( find . |
+		grep -P -i '^./(?!wezterm.lua)' |
+		grep -P -i $(echo '^./(?!'$SCRIPT_NAME')') |
+		grep -P -i '^./.*(\.).*$' |
+		sed 's/^..//'
+	)
+)
+# echo 'selected file:'
+# echo ${(F)MODULE_FILES}
+
+function copy(){
+	# copy main script
+	cp -vr "$SCRIPT_DIR/wezterm.lua" "$HOST_USERPATH/.wezterm.lua"
+
+	# copy modules
+	for value in "${MODULE_FILES[@]}"
+	do
+		mkdir -p $(dirname "$HOST_USERPATH/.wezterm/$value")
+		cp -v "$SCRIPT_DIR/$value" "$HOST_USERPATH/.wezterm/$value"
+	done
+}
+
+function watcher(){
+		quit_hint
+	while inotifywait $SCRIPT_DIR/**; do
+		copy
+		quit_hint
+	done
+}
+
+function quit_hint(){
+		echo "Press <C-c> to quit"
+}
+
+function start(){
+	copy
+	watcher
+}
+start
