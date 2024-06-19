cc6c5cfb8c3e20b9290abe3a340c884b3aeed62c
Phurinut Waranich <phurinut.wara@gmail.com>
2024-06-19 12:04:02 +0700

    wezterm: change default fontsize to 15pt both OS

--- config/wezterm/font/macOS.lua
+++ config/wezterm/font/macOS.lua
@@ -8,7 +8,7 @@ function module.apply_to_config(config)
 		{ family = "Symbols Nerd Font Mono", scale = 0.75 },
 		{ family = "Ayuthaya", scale = 1.0 },
 	})
-	config.font_size = 16.0
+	config.font_size = 15.0
 end
 
 return module
