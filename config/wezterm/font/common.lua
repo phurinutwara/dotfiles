214f6dcaa1e21daff293103773bad5797d1211e9
Phurinut Waranich <phurinut.wara@gmail.com>
2024-06-22 20:37:27 +0700

    wezterm: adjust for Windows PC

--- config/wezterm/font/common.lua
+++ config/wezterm/font/common.lua
@@ -4,7 +4,6 @@ local module = {}
 
 function module.apply_to_config(config)
 	config.adjust_window_size_when_changing_font_size = false
-	config.dpi = 110.0
 end
 
 return module
--- config/wezterm/ui/windows.lua
+++ config/wezterm/ui/windows.lua
@@ -6,7 +6,7 @@ function module.apply_to_config(config)
 	config.window_background_opacity = 0.75
 	config.win32_system_backdrop = "Auto"
 	config.enable_tab_bar = true
-	config.hide_tab_bar_if_only_one_tab = true
+	config.hide_tab_bar_if_only_one_tab = false
 end
 
 return module
