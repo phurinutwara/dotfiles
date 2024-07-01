0007b088e7835480b48dfec6addc16a983339a18
Phurinut Waranich <phurinut.wara@gmail.com>
2024-07-02 01:17:17 +0700

    i3: update config
    i3: add -b flag to i3 to ensure that it runs in background

--- config/polybar/launch_polybar.sh
+++ config/polybar/launch_polybar.sh
@@ -1,7 +1,7 @@
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload toph &
+    MONITOR=$m polybar --reload pwarch &
   done
 else
-  polybar --reload toph &
+  polybar --reload pwarch &
 fi
