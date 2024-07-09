7d91f395b7a98e77829dc24ad4c4fe0fc9c0e4e6
Phurinut Waranich <phurinut.wara@gmail.com>
2024-07-09 09:10:06 +0700

    arch: improve dump script to be faster (parallel)

--- arch/scripts/separate_packages.sh
+++ arch/scripts/separate_packages.sh
@@ -2,26 +2,54 @@
 
 SCRIPT_NAME="$(basename $0)"
 SCRIPT_DIR="$(realpath $(dirname $0))"
-# pacman -Qqe > combined_list.txt
+
+ARGS=$@
+
+C_BLUE='\033[0;34m'
+C_LIGHT_GREEN='\033[0;32m'
+NC='\033[0m'
 
 # Combined list of all packages
 combined_list="$(realpath $SCRIPT_DIR/../pkgs/combined-list.txt)"
+# pacman -Qqe > combined_list.txt
 
 # Files to store the separated package lists
 pkglist="$(realpath $SCRIPT_DIR/../pkgs/pacman/pkglist.txt)"
 aurlist="$(realpath $SCRIPT_DIR/../pkgs/yay/aurlist.txt)"
 
 # Empty the output files
-> $pkglist
-> $aurlist
+>$pkglist
+>$aurlist
 
-while read -r package; do
-    if pacman -Si "$package" &> /dev/null; then
-        echo "$package" >> $pkglist
-    else
-        echo "$package" >> $aurlist
-    fi
-done < "$combined_list"
+# DEPRECATED BUT WORKS
+if [[ $ARGS != '--old' ]]; then
+    # Multiple line: https://stackoverflow.com/questions/23929235/multi-line-string-with-extra-space-preserved-indentation
+    echo 'parallel dumping...'
+    read -r -d '' OPERATION <<-EOF
+    pacman -Si %s 1>/dev/null 2>&1 &&
+    (
+        echo '%s: ${C_BLUE}Arch${NC} pkg (official)';
+        echo %s >> $pkglist )
+    || (
+        echo '%s: ${C_LIGHT_GREEN}AUR${NC} pkg';
+        echo %s >> $aurlist )
+EOF
+    cat $combined_list | xargs -P 0 -I %s bash -c "$(echo -e $OPERATION)"
+
+    cat $pkglist | sort --unique --output=$pkglist
+    cat $aurlist | sort --unique --output=$aurlist
+elif [[ $ARGS == '--old' ]]; then
+    echo 'line-by-line dumping...'
+    while read -r package; do
+        if pacman -Si "$package" &>/dev/null; then
+            echo -e "$package: ${C_BLUE}Arch${NC} pkg (official)"
+            echo "$package" >>$pkglist
+        else
+            echo -e "$package: ${C_LIGHT_GREEN}AUR${NC} pkg"
+            echo "$package" >>$aurlist
+        fi
+    done <"$combined_list"
+fi
 
 echo "Official repository packages saved to $pkglist"
 echo "AUR packages saved to $aurlist"
