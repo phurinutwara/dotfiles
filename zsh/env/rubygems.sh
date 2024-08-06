9cd8e2eddeb76a9d4f9a606c2a54f400e2a3cb24
Phurinut Waranich <phurinut.wara@gmail.com>
2024-08-07 02:54:39 +0700

    zsh: add rubygems

---
+++ zsh/env/rubygems.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+# export GEM_PATH="$(realpath ~)/.local/share/gem/ruby/3.0.0/bin"
+# export PATH="$GEM_PATH:$PATH"
+
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+export PATH="$PATH:$HOME/.rvm/bin"
