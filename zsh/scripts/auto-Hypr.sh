9c501563ee3ca468cad3f86aa0c01b55e6d382af
Phurinut Waranich <phurinut.wara@gmail.com>
2024-08-13 22:46:20 +0700

    hyprland: move logs folder into .cache

--- zsh/scripts/auto-Hypr.sh
+++ zsh/scripts/auto-Hypr.sh
@@ -2,6 +2,7 @@
 
 # Auto start Hyprland on tty1
 if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" -eq 1 ]; then
-	mkdir -p ~/.cache
-	exec Hyprland >~/logs/hyprland.log 2>&1
+	mkdir -p ~/.cache/logs
+	echo 'Starting Hyprland...'
+	exec Hyprland >~/.cache/logs/hyprland.log 2>&1
 fi
