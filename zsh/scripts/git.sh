b190535802f5886a7c9bbcac475a8e693dbbf9be
Phurinut Waranich <phurinut.wara@gmail.com>
2024-08-29 16:04:09 +0700

    git-script: fix gopr and gopen for 'ssh://' protocol

--- zsh/scripts/git.sh
+++ zsh/scripts/git.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env zsh
 
 if [[ $ID == "arch" ]]; then
-  alias gopen='git config --get remote.origin.url | perl -pe "s/:(?!\/\/)/\//g" | sed "s/^git@/https:\/\//" | sed "s/.git$//" | xargs -I % echo "xdg-open %" | sh'
+  alias gopen='git config --get remote.origin.url | perl -pe "s/:(?!\/\/)/\//g" | sed "s/.*git@/https:\/\//" | sed "s/.git$//" | xargs -I % echo "xdg-open %" | sh'
   alias gopr='
     git config --get remote.origin.url | \
-    perl -pe "s/:(?!\/\/)/\//g" | sed "s/^git@/https:\/\//" | \
+    perl -pe "s/:(?!\/\/)/\//g" | sed "s/.*git@/https:\/\//" | \
     sed "s/.git$//" | \
     xargs -I _ echo "xdg-open _/pull-requests/new?source=$(git branch --show-current)\&dest=develop" | sh'
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  alias gopen='git config --get remote.origin.url | perl -pe "s/:(?!\/\/)/\//g" | sed "s/^git@/https:\/\//" | sed "s/.git$//" | xargs -I % echo "sensible-browser %" | sh'
+  alias gopen='git config --get remote.origin.url | perl -pe "s/:(?!\/\/)/\//g" | sed "s/.*git@/https:\/\//" | sed "s/.git$//" | xargs -I % echo "sensible-browser %" | sh'
   alias gopr='
     git config --get remote.origin.url | \
-    perl -pe "s/:(?!\/\/)/\//g" | sed "s/^git@/https:\/\//" | \
+    perl -pe "s/:(?!\/\/)/\//g" | sed "s/.*git@/https:\/\//" | \
     sed "s/.git$//" | \
     xargs -I _ echo "sensible-browser _/pull-requests/new?source=$(git branch --show-current)\&dest=develop" | sh'
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-  alias gopen='git config --get remote.origin.url | perl -pe "s/:(?!\/\/)/\//g" | sed "s/^git@/https:\/\//" | sed "s/.git$//" | xargs -I % echo "open %" | sh'
+  alias gopen='git config --get remote.origin.url | perl -pe "s/:(?!\/\/)/\//g" | sed "s/.*git@/https:\/\//" | sed "s/.git$//" | xargs -I % echo "open %" | sh'
   alias gopr='
     git config --get remote.origin.url | \
     perl -pe "s/:(?!\/\/)/\//g" | \
-    sed "s/^git@/https:\/\//" | \
+    sed "s/.*git@/https:\/\//" | \
     sed "s/.git$//" | \
     xargs -I _ echo "open _/pull-requests/new?source=$(git branch --show-current)\&dest=develop" | sh'
 fi
