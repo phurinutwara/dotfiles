cc4b10c6fdcebcdd5bd174a14e1b9cdd924101a0
Phurinut Waranich <phurinut.wara@gmail.com>
2024-09-08 01:29:34 +0700

    hyprland: adjust border color
    specific by mode

--- config/hypr/scripts/gamemode.sh
+++ config/hypr/scripts/gamemode.sh
@@ -8,6 +8,8 @@ if [ "$HYPRGAMEMODE" = 1 ]; then
         keyword general:gaps_in 0;\
         keyword general:gaps_out 0;\
         keyword general:border_size 1;\
+        keyword general:col.active_border rgb(11111C);\
+        keyword general:col.inactive_border rgb(11111C);\
         keyword decoration:rounding 0;\
         keyword decoration:active_opacity 1;
         keyword decoration:inactive_opacity 1;
