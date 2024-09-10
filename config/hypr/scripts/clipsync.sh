aa8c514432061ef92281ebfd88328746efaa8257
Phurinut Waranich <phurinut.wara@gmail.com>
2024-09-10 16:18:41 +0700

    hyprland: write script to sync wayland -> x11 clipboard

---
+++ config/hypr/scripts/clipsync.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+# One-way clipboard syncronization from Wayland to X11.
+# Requires: wl-clipboard, xclip, clipnotify.
+#
+# Usage:
+#   clipsync.sh watch - run in background.
+#   clipsync.sh kill - kill all background processes.
+#   echo -n any | clipsync.sh insert - insert clipboard content fron stdin.
+#
+# Workaround for issue:
+# "Clipboard synchronization between wayland and xwayland clients broken"
+# https://github.com/hyprwm/Hyprland/issues/6132
+
+# Updates clipboard content of both Wayland and X11 if current clipboard content differs.
+# Usage: echo -e "1\n2" | clipsync insert
+
+watch() {
+    # Wayland -> X11
+    while clipnotify; do
+        wl-paste | xclip -r -selection clipboard
+    done &
+}
+
+kill() {
+    pkill -e wl-paste
+    pkill -e clipnotify
+    pkill -e xclip
+    pkill -e clipsync
+}
+
+"$@"
