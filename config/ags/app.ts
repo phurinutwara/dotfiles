a07b0cc7e21a26bcfd235a22dc2cb1594438021e
Phurinut Waranich <phurinut.wara@gmail.com>
2024-11-24 22:29:50 +0700

    ags: writing some simple usable bar

--- config/ags/app.ts
+++ config/ags/app.ts
@@ -1,10 +1,10 @@
 import { App } from "astal/gtk3"
-import style from "./style.scss"
-import Bar from "./widget/Bar"
+import style from "./scss/main.scss"
+import Bar from "./widget/bar/main"
 
 App.start({
-    css: style,
-    main() {
-        App.get_monitors().map(Bar)
-    },
+  css: style,
+  main() {
+    App.get_monitors().map(Bar)
+  },
 })
