cfb42a47d9269676117c275400cf62728cc3901d
Phurinut Waranich <phurinut.wara@gmail.com>
2024-12-10 06:14:24 +0700

    ags: temporary fix from ags to ags.v1

--- config/ags.v1/scripts/color_generation/colorgen.sh
+++ config/ags.v1/scripts/color_generation/colorgen.sh
@@ -3,7 +3,7 @@
 XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
 XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
-CONFIG_DIR="$XDG_CONFIG_HOME/ags"
+CONFIG_DIR="$XDG_CONFIG_HOME/ags.v1"
 CACHE_DIR="$XDG_CACHE_HOME/ags"
 STATE_DIR="$XDG_STATE_HOME/ags"
 
@@ -18,28 +18,28 @@ colormodefile="$STATE_DIR/user/colormode.txt"
 lightdark="dark"
 transparency="opaque"
 materialscheme="vibrant"
-terminalscheme="$XDG_CONFIG_HOME/ags/scripts/templates/terminal/scheme-base.json"
+terminalscheme="$XDG_CONFIG_HOME/ags.v1/scripts/templates/terminal/scheme-base.json"
 
 if [ ! -f $colormodefile ]; then
-    echo "dark" > $colormodefile
-    echo "opaque" >> $colormodefile
-    echo "vibrant" >> $colormodefile
-elif [[ $(wc -l < $colormodefile) -ne 4 || $(wc -w < $colormodefile) -ne 4 ]]; then
-    echo "dark" > $colormodefile
-    echo "opaque" >> $colormodefile
-    echo "vibrant" >> $colormodefile
-    echo "yesgradience" >> $colormodefile
+    echo "dark" >$colormodefile
+    echo "opaque" >>$colormodefile
+    echo "vibrant" >>$colormodefile
+elif [[ $(wc -l <$colormodefile) -ne 4 || $(wc -w <$colormodefile) -ne 4 ]]; then
+    echo "dark" >$colormodefile
+    echo "opaque" >>$colormodefile
+    echo "vibrant" >>$colormodefile
+    echo "yesgradience" >>$colormodefile
 else
     lightdark=$(sed -n '1p' $colormodefile)
     transparency=$(sed -n '2p' $colormodefile)
     materialscheme=$(sed -n '3p' $colormodefile)
     if [ "$materialscheme" = "monochrome" ]; then
-      terminalscheme="$XDG_CONFIG_HOME/ags/scripts/templates/terminal/scheme-monochrome.json"
+        terminalscheme="$XDG_CONFIG_HOME/ags.v1/scripts/templates/terminal/scheme-monochrome.json"
     fi
 fi
 backend="material" # color generator backend
 if [ ! -f "$STATE_DIR/user/colorbackend.txt" ]; then
-    echo "material" > "$STATE_DIR/user/colorbackend.txt"
+    echo "material" >"$STATE_DIR/user/colorbackend.txt"
 else
     backend=$(cat "$STATE_DIR/user/colorbackend.txt") # either "" or "-l"
 fi
@@ -47,9 +47,9 @@ fi
 cd "$CONFIG_DIR/scripts/" || exit
 if [[ "$1" = "#"* ]]; then # this is a color
     color_generation/generate_colors_material.py --color "$1" \
-    --mode "$lightdark" --scheme "$materialscheme" --transparency "$transparency" \
-    --termscheme $terminalscheme --blend_bg_fg \
-    > "$CACHE_DIR"/user/generated/material_colors.scss
+        --mode "$lightdark" --scheme "$materialscheme" --transparency "$transparency" \
+        --termscheme $terminalscheme --blend_bg_fg \
+        >"$CACHE_DIR"/user/generated/material_colors.scss
     if [ "$2" = "--apply" ]; then
         cp "$CACHE_DIR"/user/generated/material_colors.scss "$STATE_DIR/scss/_material.scss"
         color_generation/applycolor.sh
@@ -60,10 +60,10 @@ elif [ "$backend" = "material" ]; then
         smartflag='--smart'
     fi
     color_generation/generate_colors_material.py --path "$1" \
-    --mode "$lightdark" --scheme "$materialscheme" --transparency "$transparency" \
-    --termscheme $terminalscheme --blend_bg_fg \
-    --cache "$STATE_DIR/user/color.txt" $smartflag \
-    > "$CACHE_DIR"/user/generated/material_colors.scss
+        --mode "$lightdark" --scheme "$materialscheme" --transparency "$transparency" \
+        --termscheme $terminalscheme --blend_bg_fg \
+        --cache "$STATE_DIR/user/color.txt" $smartflag \
+        >"$CACHE_DIR"/user/generated/material_colors.scss
     if [ "$2" = "--apply" ]; then
         cp "$CACHE_DIR"/user/generated/material_colors.scss "$STATE_DIR/scss/_material.scss"
         color_generation/applycolor.sh
@@ -75,16 +75,16 @@ elif [ "$backend" = "pywal" ]; then
     # copy scss
     cp "$XDG_CACHE_HOME/wal/colors.scss" "$CACHE_DIR"/user/generated/material_colors.scss
 
-    cat color_generation/pywal_to_material.scss >> "$CACHE_DIR"/user/generated/material_colors.scss
+    cat color_generation/pywal_to_material.scss >>"$CACHE_DIR"/user/generated/material_colors.scss
     if [ "$2" = "--apply" ]; then
         sass -I "$STATE_DIR/scss" -I "$CONFIG_DIR/scss/fallback" "$CACHE_DIR"/user/generated/material_colors.scss "$CACHE_DIR"/user/generated/colors_classes.scss --style compressed
         sed -i "s/ { color//g" "$CACHE_DIR"/user/generated/colors_classes.scss
         sed -i "s/\./$/g" "$CACHE_DIR"/user/generated/colors_classes.scss
         sed -i "s/}//g" "$CACHE_DIR"/user/generated/colors_classes.scss
         if [ "$lightdark" = "-l" ]; then
-            printf "\n""\$darkmode: false;""\n" >> "$CACHE_DIR"/user/generated/colors_classes.scss
+            printf "\n""\$darkmode: false;""\n" >>"$CACHE_DIR"/user/generated/colors_classes.scss
         else
-            printf "\n""\$darkmode: true;""\n" >> "$CACHE_DIR"/user/generated/colors_classes.scss
+            printf "\n""\$darkmode: true;""\n" >>"$CACHE_DIR"/user/generated/colors_classes.scss
         fi
 
         cp "$CACHE_DIR"/user/generated/colors_classes.scss "$STATE_DIR/scss/_material.scss"
--- config/ags.v1/scripts/color_generation/switchcolor.sh
+++ config/ags.v1/scripts/color_generation/switchcolor.sh
@@ -3,7 +3,7 @@
 XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
 XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
-CONFIG_DIR="$XDG_CONFIG_HOME/ags"
+CONFIG_DIR="$XDG_CONFIG_HOME/ags.v1"
 CACHE_DIR="$XDG_CACHE_HOME/ags"
 STATE_DIR="$XDG_STATE_HOME/ags"
 
@@ -20,16 +20,16 @@ fi
 sed -i "1s/.*/$color/" "$STATE_DIR/user/color.txt"
 
 # Use Gradience?
-colormodelines=$(wc -l "$COLORMODE_FILE_DIR"  | awk '{print $1}' )
+colormodelines=$(wc -l "$COLORMODE_FILE_DIR" | awk '{print $1}')
 if [ "$2" == "--no-gradience" ]; then
   if [ "$colormodelines" == "3" ]; then
-    echo 'nogradience' >> "$COLORMODE_FILE_DIR"
+    echo 'nogradience' >>"$COLORMODE_FILE_DIR"
   else
     sed -i "4s/.*/nogradience/" "$COLORMODE_FILE_DIR"
   fi
 elif [ "$2" == "--yes-gradience" ]; then
   if [ "$colormodelines" == "3" ]; then
-    echo 'yesgradience' >> "$COLORMODE_FILE_DIR"
+    echo 'yesgradience' >>"$COLORMODE_FILE_DIR"
   else
     sed -i "4s/.*/yesgradience/" "$COLORMODE_FILE_DIR"
   fi
