6d7a1c6490faece7c0f85aaeaaa6d2eb09c4b1d4
Phurinut Waranich <phurinut.wara@gmail.com>
2024-12-20 15:12:55 +0700

    dotfiles: made entries for wsl

--- zsh/scripts/brew.sh
+++ zsh/scripts/brew.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
 
-# TODO: EXECPT ARCH OS HERE
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  export BREWFILE_PATH="~/dotfiles/homebrew/macos/Brewfile"
+  export BREWFILE_PATH="$HOME/dotfiles/brew/macos/Brewfile"
+  mkdir -p "$(dirname "$BREWFILE_PATH")"
   eval "$(/usr/local/bin/brew shellenv)"
+elif [[ $(uname -r) == *'WSL'* ]]; then
+  export BREWFILE_PATH="$HOME/dotfiles/linux/WSL/Brewfile"
+  mkdir -p "$(dirname "$BREWFILE_PATH")"
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  export BREWFILE_PATH="~/dotfiles/homebrew/linux/Brewfile"
+  export BREWFILE_PATH="$HOME/dotfiles/linux/$ID/Brewfile"
+  mkdir -p "$(dirname "$BREWFILE_PATH")"
+  # TODO: eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 
 function bbd() {
-  if [[ "$OSTYPE" == "darwin"* ]]; then
-    brew bundle dump --force --describe --file "$BREWFILE_PATH" &&
-      zsh -c "echo \"\" >> $BREWFILE_PATH && echo -e \"# vim:ft=ruby\" >> $BREWFILE_PATH"
-  elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    brew bundle dump --force --describe --file "$BREWFILE_PATH" &&
-      zsh -c "echo \"\" >> $BREWFILE_PATH && echo -e \"# vim:ft=ruby\" >> $BREWFILE_PATH"
-  else
+  if [[ ("$OSTYPE" != "darwin"* && "$OSTYPE" != "linux-gnu"*) ]]; then
     echo "Unknown \$OSTYPE, aborting process"
+  else
+    brew bundle dump --force --describe --file "$BREWFILE_PATH"
+    zsh -c "echo \"\" >> $BREWFILE_PATH && echo -e \"# vim:ft=ruby\" >> $BREWFILE_PATH"
   fi
 }
--- zsh/scripts/git.sh
+++ zsh/scripts/git.sh
@@ -7,13 +7,13 @@ if [[ $ID == "arch" ]]; then
     perl -pe "s/:(?!\/\/)/\//g" | sed "s/.*git@/https:\/\//" | \
     sed "s/.git$//" | \
     xargs -I _ echo "xdg-open _/pull-requests/new?source=$(git branch --show-current)\&dest=develop" | sh'
-elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  alias gopen='git config --get remote.origin.url | perl -pe "s/:(?!\/\/)/\//g" | sed "s/.*git@/https:\/\//" | sed "s/.git$//" | xargs -I % echo "sensible-browser %" | sh'
+elif [[ $(uname -r) == *'WSL'* ]]; then
+  alias gopen='git config --get remote.origin.url | perl -pe "s/:(?!\/\/)/\//g" | sed "s/.*git@/https:\/\//" | sed "s/.git$//" | xargs -I % echo "explorer.exe %" | sh'
   alias gopr='
     git config --get remote.origin.url | \
     perl -pe "s/:(?!\/\/)/\//g" | sed "s/.*git@/https:\/\//" | \
     sed "s/.git$//" | \
-    xargs -I _ echo "sensible-browser _/pull-requests/new?source=$(git branch --show-current)\&dest=develop" | sh'
+    xargs -I _ echo "explorer.exe _/pull-requests/new?source=$(git branch --show-current)\&dest=develop" | sh'
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   alias gopen='git config --get remote.origin.url | perl -pe "s/:(?!\/\/)/\//g" | sed "s/.*git@/https:\/\//" | sed "s/.git$//" | xargs -I % echo "open %" | sh'
   alias gopr='
